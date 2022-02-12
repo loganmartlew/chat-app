2abe8b1a54355968591d8f62370acf6333dc2648
Logan Martlew <logan.martlew@gmail.com>
2022-02-12 17:24:39 +1300

    Added signUp prop to TitleText render in AuthPage

--- src/components/AuthPage/index.tsx
+++ src/components/AuthPage/index.tsx
@@ -17,7 +17,7 @@ const AuthPage: FC<Props> = ({ onSubmit, signUp }) => {
       sx={{ height: '100vh', display: 'grid', placeContent: 'center' }}
     >
       <Box sx={{ width: 'max-content', mx: 'auto' }}>
-        <TitleText />
+        <TitleText signUp={signUp} />
         <AuthForm signUp={signUp} onSubmit={onSubmit} />
         <Divider sx={{ my: 4 }}>OR</Divider>
         <SocialLogins />
